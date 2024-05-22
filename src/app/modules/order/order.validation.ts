8da9c894263204e0751409e9ee38329e2dd9b737
fahadhasan164 <fahadhasan164@gmail.com>
2024-05-22 12:30:48 +0600

    zod order validation

--- src/app/modules/order/order.validation.ts
+++ src/app/modules/order/order.validation.ts
@@ -1,1 +1,11 @@
-// lllllllllllll
+import { z } from "zod";
+// order zod validation
+
+const zodOrderValidation = z.object({
+    email: z.string().email(),
+    productId: z.string(),
+    price: z.number(),
+    quantity: z.number()
+})
+
+export default zodOrderValidation;
