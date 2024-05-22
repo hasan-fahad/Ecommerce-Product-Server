60402ab593088a023bd2d4fc3c5c726d69fe38c9
fahadhasan164 <fahadhasan164@gmail.com>
2024-05-22 13:08:09 +0600

    handler add

--- src/app.ts
+++ src/app.ts
@@ -18,4 +18,20 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
+// Route not found handler
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+app.use((err: any, req: Request, res: Response) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+  });
+});
+
 export default app;
